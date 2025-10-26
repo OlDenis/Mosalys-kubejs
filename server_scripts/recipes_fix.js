3199a7aa476742fd485d516791987102be35918b
Olivier <olivierdenis99@hotmail.com>
2025-10-26 00:26:49 -0400

    Adjusted crushed tin pile recipe

--- server_scripts/recipes_fix.js
+++ server_scripts/recipes_fix.js
@@ -3,7 +3,16 @@
 ServerEvents.recipes(event => {
   event.remove({id: 'spelunkers_palette:iron/iron_trapdoor'})
   event.remove({id: 'create:crushing/scrap_cobblestone_small'})
-
-
+  
+  event.shapeless(
+    'create_compressed:crushed_tin_pile',
+    '9x create_ironworks:crushed_raw_tin'
+  ) 
+  event.shapeless(
+    '9x create_ironworks:crushed_raw_tin',
+    'create_compressed:crushed_tin_pile'
+  ) 
+  event.remove({id: 'create_compressed:crushed_raw_tin'})
+  
 }
 )
