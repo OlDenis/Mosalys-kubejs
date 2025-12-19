fb9f1ba348afb3dd3840ff49e89c10a440ea621c
Olivier <olivierdenis99@hotmail.com>
2025-12-19 00:46:19 -0500

    fix beehives duplicates

--- server_scripts/duplicates.js
+++ server_scripts/duplicates.js
@@ -580,6 +580,19 @@ ServerEvents.recipes(event => {
         ]
     )
 
+    // Beehives
+    for (const wood of ['spruce', 'birch', 'jungle', 'acacia', 'dark_oak', 'mangrove', 'crimson', 'warped']){
+        event.shapeless(
+            `friendsandfoes:${wood}_beehive`,
+            `woodworks:${wood}_beehive`
+        )
+        event.shapeless(
+            `woodworks:${wood}_beehive`,
+            `friendsandfoes:${wood}_beehive`
+        )
+        event.remove({id: `friendsandfoes:${wood}_beehive`})
+    }
+
     // THIS LINE IS ALSO IMPORTANT!
     // IT MUST BE THE LAST LINE IN THE EVENT HANDLER
     event.recipes.create.finalize();
